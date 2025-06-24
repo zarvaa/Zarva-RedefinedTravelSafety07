89f2aeca0e30ec32551349ae5c84a6b5c3f3349b
Sanjna07 <sanjanatripathi709@gmail.com>
2025-06-25 00:04:07 +0530

    new change

--- Server/server.js
+++ Server/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Your frontend's URL
+    origin: "http://localhost:5174", // Your frontend's URL
     credentials: true, // Allow cookies to be sent
   })
 );
